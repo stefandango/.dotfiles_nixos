424e87d186f58d206a76bb2822bff26f928d3926
Stefan <5796872+stefandango@users.noreply.github.com>
2024-01-13 12:13:36 +0100

    Added development tools (rider + dotnetsdk8)

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, unstable, vars, ...}:
 {
    environment.systemPackages = with pkgs; [
+	dotnet-sdk_8
 	obsidian
+	jetbrains.rider
     ];
   nixpkgs.config.permittedInsecurePackages = [
                 "electron-25.9.0"
