6580127aa3991fff7fba04cf254f9be317d2988d
Stefan <5796872+stefandango@users.noreply.github.com>
2024-01-17 21:10:31 +0100

    cleanup of xdg files

---
+++ modules/nvim.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, ... }:
+
+let
+	colors = import ../theme/colors.nix;
+in
+{
+	programs.nixvim = {
+		enable = true;
+		viAlias = true;
+		vimAlias = true;
+		defaultEditor = true;
+	};
+}
