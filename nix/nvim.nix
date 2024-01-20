154fa9c791b9e29e86be407b853e7f8f6a4a26b4
Stefan <5796872+stefandango@users.noreply.github.com>
2024-01-20 22:32:39 +0100

    Initial nvim setup

--- nix/nvim.nix
+++ nix/nvim.nix
@@ -9,5 +9,67 @@ in
 		viAlias = true;
 		vimAlias = true;
 		defaultEditor = true;
+		colorschemes.rose-pine = {
+			enable = true;
+			package = pkgs.vimPlugins.rose-pine;
+			transparentBackground = true;
+		};
+		autoCmd = [
+		{
+			event = "VimEnter";
+			command = "set nofoldenable";
+			desc = "Unfold All";
+		}
+		{
+			event = "BufWrite";
+			command = "%s/\\s\\+$//e";
+			desc = "Remove Whitespaces";
+		}
+		];
+		options = {
+			number = true;
+			relativenumber = true;
+			hidden = true;
+			foldlevel = 99;
+			tabstop = 4;
+			softtabstop = 4;
+			shiftwidth = 4;
+			expandtab = true;
+			autoindent = true;
+			wrap = false;
+			scrolloff = 8;
+			sidescroll = 40;
+			completeopt = ["menu" "menuone" "noselect"];
+			pumheight = 15;
+			swapfile = false;
+			timeoutlen = 2500;
+			conceallevel = 3;
+			undofile = true;
+			ignorecase = true;
+			smartcase = true;
+		};
+		extraConfigLua = ''
+			vim.opt.smartcase = true
+			vim.opt.ignorecase = true
+			vim.opt.hlsearch = false
+			vim.opt.incsearch = true
+			vim.opt.mouse = 'a'
+			vim.opt.signcolumn = "yes"
+			vim.opt.isfname:append("@-@")
+
+			vim.opt.colorcolumn = "120"
+			vim.opt.cmdheight = 0
+			vim.opt.shortmess:append('I')
+
+		'';
+		clipboard = {
+			register = "unnamedplus";
+			providers.wl-copy.enable = true;
+		};
+		
+		globals = {
+			mapleader = " ";
+			maplocalleader = " ";
+		};
 	};
 }
