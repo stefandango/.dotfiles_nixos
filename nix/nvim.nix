83fb6e7cb255d2fc296fa44ebd181a6bed5af813
Stefan <5796872+stefandango@users.noreply.github.com>
2024-01-22 15:58:57 +0100

    Updated nvimtree as popup

--- nix/nvim.nix
+++ nix/nvim.nix
@@ -55,13 +55,6 @@ in
             cmdheight = 0;
 
 		};
-		extraConfigLua = ''
-			vim.opt.isfname:append("@-@")
-			vim.opt.shortmess:append('I')
-
-            require('neoscroll').setup()
-
-		'';
 		clipboard = {
 			register = "unnamedplus";
 			providers.wl-copy.enable = true;
@@ -72,27 +65,71 @@ in
 			maplocalleader = " ";
 		};
 
-        plugins = {
-            nvim-tree = {
-                enable = true;
-                sortBy = "case_sensitive";
-                view = {
-                    relativenumber = true;
-                    number = true;
-                    float = {
-                        enable = true;
-                        openWinConfig = {
-                            border = "rounded";
-                            relative = "editor";
-                        };
-                    };
+        keymaps = [
+        {
+            key = "<leader>.";
+            action = "<CMD>NvimTreeToggle<CR>";
+            options.desc = "Toggle NeoTree";
+        }
+        ];
 
-                };
-            };
+        plugins = {
         };
 
         extraPlugins = with pkgs.vimPlugins; [
             neoscroll-nvim
+            nvim-tree-lua
         ];
+
+		extraConfigLua = ''
+			vim.opt.isfname:append("@-@")
+			vim.opt.shortmess:append('I')
+
+            require('neoscroll').setup()
+
+            local HEIGHT_RATIO = 0.8  -- You can change this
+            local WIDTH_RATIO = 0.5   -- You can change this too
+
+            require("nvim-tree").setup({
+                    sort_by = "case_sensitive",
+                    view = {
+                    relativenumber = true,
+                    number = true,
+                    side = "right",
+                    width = 100,
+                    float = {
+                    enable = true,
+                    open_win_config = function()
+                    local screen_w = vim.opt.columns:get()
+                    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                    local window_w = screen_w * WIDTH_RATIO
+                    local window_h = screen_h * HEIGHT_RATIO
+                    local window_w_int = math.floor(window_w)
+                    local window_h_int = math.floor(window_h)
+                    local center_x = (screen_w - window_w) / 2
+                    local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                    - vim.opt.cmdheight:get()
+                    return {
+                        border = 'rounded',
+                        relative = 'editor',
+                        row = center_y,
+                        col = center_x,
+                        width = window_w_int,
+                        height = window_h_int,
+                    }
+                    end,
+                    },
+                    },
+                    renderer = {
+                        group_empty = true,
+                        highlight_git = true
+                    },
+                    filters = {
+                        dotfiles = false,
+                    },
+            })
+
+		'';
+
 	};
 }
