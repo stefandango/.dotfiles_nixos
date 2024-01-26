ca90320003713a719a50dec0b5fd84a4b7f93ab7
Stefan <5796872+stefandango@users.noreply.github.com>
2024-01-26 16:18:02 +0100

    added steam :)

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,10 +1,13 @@
 { config, lib, pkgs, unstable, vars, ...}:
 {
-   environment.systemPackages = with pkgs; [
-	obsidian
-	jetbrains.rider
+    environment.systemPackages = with pkgs; [
+        obsidian
+        jetbrains.rider
+        vscode-fhs
     ];
-  nixpkgs.config.permittedInsecurePackages = [
-                "electron-25.9.0"
-              ];
+    nixpkgs.config.permittedInsecurePackages = [
+        "electron-25.9.0"
+    ];
+    programs.steam.enable = true;
+
 }
