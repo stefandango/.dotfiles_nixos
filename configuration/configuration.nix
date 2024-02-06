92fc43e7aed6ac887c658b2b70b07d20387dbaca
Stefan <5796872+stefandango@users.noreply.github.com>
2024-02-06 20:53:03 +0100

    added pinentry (dependency for gpg)

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -152,6 +152,7 @@ in
 		  unzip
           zip
           gnupg
+          pinentry
 	      xdg-ninja
 
 # VIDEO/AUDIO
@@ -212,10 +213,10 @@ in
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+   programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
 
   # List services that you want to enable:
 
