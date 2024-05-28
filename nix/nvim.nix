c5b7ba6fc462d93251ef56c26a1ef36909db3380
Stefan <5796872+stefandango@users.noreply.github.com>
2024-05-28 22:35:42 +0200

    Working on neotest / dotnet testing in nvim

--- nix/nvim.nix
+++ nix/nvim.nix
@@ -562,18 +562,25 @@
                 suggestion.enabled = false;
             };
             copilot-cmp.enable = true;
-            neotest = {
-                enable = true;
-                adapters = {
-                    dotnet = {
-                        enable = true;
-                    };
-                };
-                settings = {
-                    #default_strategy = "dap";
-                    log_level = "info";
-                };
-            };
+            # neotest = {
+            #     enable = true;
+            #     adapters = {
+            #         dotnet = {
+            #             enable = true;
+            #             settings = {
+            #                 discovery_root = "solution";
+            #             };
+            #         };
+            #         plenary = {
+            #             enable = true;
+            #         };
+            #     };
+            #     settings = {
+            #         #default_strategy = "dap";
+            #         log_level = "info";
+            #
+            #     };
+            # };
         };
 
         extraPlugins = with pkgs.vimPlugins; [
@@ -584,11 +591,11 @@
             nvim-dap-ui
             CopilotChat-nvim
             #plenary-nvim
-            #nvim-nio
-            #FixCursorHold-nvim
-            #neotest
-            #neotest-plenary
-            #neotest-dotnet
+            nvim-nio
+            FixCursorHold-nvim
+            neotest
+            neotest-plenary
+            neotest-dotnet
         ];
 
         extraConfigLua = ''
@@ -652,6 +659,30 @@
            require("CopilotChat").setup {
                 debug = false, -- Enable debugging
             }
+
+        require("neotest").setup({
+                adapters = {
+                require("neotest-dotnet")({
+                        dap = {
+                        -- Extra arguments for nvim-dap configuration
+                        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+                        args = {justMyCode = false },
+                        -- Enter the name of your dap adapter, the default value is netcoredbg
+                        adapter_name = "netcoredbg"
+                        },
+                        -- Let the test-discovery know about your custom attributes (otherwise tests will not be picked up)
+                        -- Provide any additional "dotnet test" CLI commands here. These will be applied to ALL test runs performed via neotest. These need to be a table of strings, ideally with one key-value pair per item.
+                        dotnet_additional_args = {
+                        "--verbosity detailed"
+                        },
+                        -- Tell neotest-dotnet to use either solution (requires .sln file) or project (requires .csproj or .fsproj file) as project root
+                        -- Note: If neovim is opened from the solution root, using the 'project' setting may sometimes find all nested projects, however,
+                        --       to locate all test projects in the solution more reliably (if a .sln file is present) then 'solution' is better.
+                            discovery_root = "solution" -- Default
+                }),
+                    require("neotest-plenary")
+                }
+        })
 
         -- DEBUGGER STUFF (VERY UNSTABLE STILL)
 
